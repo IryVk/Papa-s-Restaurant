70a78b98bafc58309a87a4539f743a14c975cf71
Arwa Essam Mesameh Abdelaziz <aa2101585@tkh.edu.eg>
2023-04-22 23:31:03 +0200

    added homepage and navbar

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Papa's Restaurant</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="color game" />
+    <link rel="stylesheet" href="styles.css" />
+    <script src="script.js" type="text/javascript"></script>
+    <style>
+        #logo {
+            width: 100px;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div><img id="logo" src="images/logo.png"></div>
+        hh
+    </div>
+
+</body>
+</html>
